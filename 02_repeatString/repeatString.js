457d978d6cbed895e3f67a2616654cd8cffaa5e9
Ifo Ikede <ifoitech@gmail.com>
2022-07-03 20:07:11 -0300

    solution for reapt Strings

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,28 @@
-const repeatString = function() {
+const repeatString = function( inputString, repeatCount ) {
+   let outputString = "" ;
+   
+    // if ( !isBlankString(inputString)) {
+        
+        if( repeatCount < 0 ) {
+            outputString = "ERROR"
+
+        }else {
+                    for (let i = 0; i < repeatCount; i++) {
+                    outputString += inputString ;
+                    }
+                }
+        // }
+  return outputString 
 
 };
+
+function isBlankString(string) {
+    return (string.length === 0 || !string.trim()) ;
+}
+
+// console.log(isBlankString("Hello Javascript"))
+// console.log(isBlankString(""))
+// console.log(isBlankString("    "))
 
 // Do not edit below this line
 module.exports = repeatString;
