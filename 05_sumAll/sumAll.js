7ccd092380119c808cdfd55834bc6e9cede1a0c0
Ifo Ikede <ifoitech@gmail.com>
2022-07-03 23:13:39 -0300

    solutions for sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,36 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+     
+  if ( !isValidInput(start, end)  ) { 
+    return "ERROR";
 
+  } else if( start > end ) {
+        temp = start ;
+        start = end ;
+        end = temp ;
+    }
+    result = start ;
+
+
+    for (var i = start +1 ; i < end + 1; i++) {
+        result += i
+    }
+  
+    return result ;
 };
+
+ function isValidInput( first, second ) {
+    if  // (Number.isNaN(first) || Number.isNaN(second) ) {
+     (!Number.isInteger(first) || !Number.isInteger(second)){
+     console.log("entered NAN section ") ;  
+    return false ;
+
+    }else if ( first < 0  || second < 0 ) { 
+        return false ;
+        
+        } else return true ;
+    
+   
+ }
 
 // Do not edit below this line
 module.exports = sumAll;
