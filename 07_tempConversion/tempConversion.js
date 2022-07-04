ec9344ac62031266199f3046739d50b86b50f448
Ifo Ikede <ifoitech@gmail.com>
2022-07-04 00:10:20 -0300

    solution to tempConversion challenge

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,14 @@
-const ftoc = function() {
+const ftoc = function(temperature) {
+      let answer = ((temperature -32) * 5/9).toFixed(1) ;
+      
+      return  parseFloat(answer)
+
 
 };
 
-const ctof = function() {
-
+const ctof = function(temperature) {
+     let answer = ((temperature * 9/5) + 32).toFixed(1) ;
+  return parseFloat(answer );
 };
 
 // Do not edit below this line
