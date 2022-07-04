0db0eac1f544c2adb1a0cb743a801843af72fc98
Ifo Ikede <ifoitech@gmail.com>
2022-07-03 21:18:28 -0300

    solution to removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    // index = array.indexOf(item);
+    // if (index !== -1) {
+    //     array.splice(index, 1);
+    // }
+       
+    return array.filter( item => !args.includes(item));
 };
 
 // Do not edit below this line
